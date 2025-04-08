732853af09e1efc8bca9cf5c88e0c6803f44f339
LeeHyunJin323 <skrk3042@naver.com>
2025-04-09 02:38:02 +0900

    20250409

---
+++ rrrr/src/codingtest/Dwarf.java
@@ -0,0 +1,37 @@
+package codingtest;
+
+import java.util.Scanner;
+
+public class Dwarf {
+
+	public static void main(String[] args) {
+		
+		// 7명이 아닌 9명이 옴
+		// 키를 합치면 100
+		int sum = 0;
+		Scanner sc = new Scanner(System.in);
+		
+		int[] dwarf = new int[9];
+ 		
+		for(int i = 0; i<9; i++) {
+			
+			dwarf[i] = sc.nextInt();
+//			System.out.println("난쟁이 키 = " +i+"번쨰"+ dwarf[i]);
+			
+			
+			// 7명 합쳐서 100이 되는 숫자면 멈춤
+			
+		}
+		
+		for (int i = 0; i< 8; i++)
+		{
+			for (int j=1; j<9; j++) {
+				
+				sum += dwarf[i] + dwarf[j];
+			
+			}
+		}
+
+	}
+
+}
